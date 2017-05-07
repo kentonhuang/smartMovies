73ea33419383d1eb8c2f5569bbb349eadf24d172
Wai Myint <sayswai@gmail.com>
2017-05-07 16:22:43 -0700

    able to fetch more than 20 movies.

--- fetcher.py
+++ fetcher.py
@@ -9,7 +9,9 @@ import movie
 class Fetcher:
 
     popular = 0
+    popular_page = 1
     theater = 0
+    theater_page = 1
     genresT = []
 
     def __init_(self):
@@ -20,40 +22,64 @@ class Fetcher:
         if movieList is None:
             return False
 
+
         if type is 'theater':
-            response = requests.get(api.base + api.theater + api.v3 + api.end)
+            response = requests.get(api.base + api.theater + api.v3 + api.end + str(self.theater_page))
             ct = self.theater
         else:
-            response = requests.get(api.base + api.popular + api.v3 + api.end)
+            response = requests.get(api.base + api.popular + api.v3 + api.end + str(self.popular_page))
             ct = self.popular
-
         if response.status_code == 200:
             data = response.json()
-            if ct < data['total_results']:
-                for x in range(0, a):
-                    name = data['results'][ct]['title']
-                    genres = []
-                    for y in data['results'][ct]['genre_ids']:
-                        gName = (b for b in api.genre if b['id'] == y).next()['name']
-                        genres.append(gName)
-                        if gName not in self.genresT:
-                            self.genresT.append(gName)
-                    date = data['results'][ct]['release_date']
-                    m = movie.Movie(name, genres, date)
-                    movieList.append(m)
-                    ct += 1
-                    if type is 'theater':
-                        self.theater += 1
-                    else:
-                        self.popular += 1
-
-                    ##print "fetch successful, # of movies in the list "+str(type) +""
-                    #print " # of genres: " +str(len(self.genresT))
-            else:
-                print "Cannot fetch anymore new movies, no more in existent"
         else:
-            print "API fetch failed, try again"
+            print "API fetch error"
             return False
+
+        x = 0
+        while x < a:
+            if ct < len(data['results']):
+                name = data['results'][ct]['original_title']
+                genres = []
+                for y in data['results'][ct]['genre_ids']:
+                    gName = (b for b in api.genre if b['id'] == y).next()['name']
+                    genres.append(gName)
+                    if gName not in self.genresT:
+                        self.genresT.append(gName)
+                date = data['results'][ct]['release_date']
+                m = movie.Movie(name, genres, date)
+                movieList.append(m)
+                if type is 'theater':
+                    self.theater += 1
+                else:
+                    self.popular += 1
+                ct += 1
+                x += 1
+                ##print "fetch successful, # of movies in the list "+str(type) +""
+                #print " # of genres: " +str(len(self.genresT))
+            else:
+                if type is 'theater':
+                    if self.theater_page > data['total_pages']:
+                        print "Cannot pull anymore movies from theater"
+                        return False
+                    self.theater_page += 1
+                    self.theater = 0
+                    response = requests.get(api.base + api.theater + api.v3 + api.end + str(self.theater_page))
+                    ct = self.theater
+                else:
+                    if self.popular_page > data['total_pages']:
+                        print "Cannot pull anymore movies from theater"
+                        return False
+                    self.popular_page += 1
+                    self.popular = 0
+                    response = requests.get(api.base + api.popular + api.v3 + api.end + str(self.popular_page))
+                    ct = self.popular
+                if response.status_code == 200:
+                    data = response.json()
+                else:
+                    print "API fetch error"
+                    return False
+
+
 
     def printMovieList(self, movieList):
         for x in movieList:
@@ -63,5 +89,4 @@ class Fetcher:
     def printGenres(self):
         print "Total Genres: " + str(len(self.genresT)) + ""
         print "Genres: " + ', '.join(self.genresT)
-
 
