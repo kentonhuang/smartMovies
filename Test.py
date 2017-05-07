a7b48d3bad936e1c51e8403741545879b3a4e0b9
Soneer Sainion <soneer.sainion@sjsu.edu>
2017-05-07 15:15:59 -0700

    Incorporates Wai's and Jorge's portion
    Known issues: popular movies stop pulling after 4 run (20 movies).

--- Test.py
+++ Test.py
@@ -1,33 +1,19 @@
-import imdb
+import HashTable as h
 import LearningElement
 import user_input
 import fetcher
 
 user = user_input.NewUser()
-user_movie_genre = user.get_genre()
-imbd_movie_list = user.get_movie_list()
-le1 = LearningElement.LearningElement(5,user_movie_genre, imbd_movie_list)
+Hash = h.HT([]) 
+foundMovie = False
+runCount = 0
+while foundMovie == False:
+    runCount = runCount+1
+    print ""
+    print "-------------------------------------------"
+    print "Run:", runCount, " Performance Measure:", user.performanceMeasure
+    user_movie_genre = user.get_genre()
+    Hash.updateProbs(user_movie_genre) 
+    print user_movie_genre
+    print Hash.Hello() 
 
-
-
-print "_________________________________________________________"
-print "Current Performance Measure:", le1.performance_measure
-print ""
-#Kentons logic, wrote this just to do a simple test 
-for x in imbd_movie_list:
-    #print "Movie: " +x.getName()+ "\n Genre: " + ' '.join(x.getGenres())+ "\n  Release Date: " +x.getDate()+ "\n"
-    for ug in user_movie_genre:
-        if ug in ' '.join(x.getGenres()):
-            print ""
-            print "----------------"
-            print x.getName()
-            print "Like/Dislike"
-            current_input =  raw_input(">")
-            if current_input == "Like":
-                le1.parseUserResponse(x, True) 
-            if current_input == "Dislike":
-                le1.parseUserResponse(x, False) 
-                
-  
-#   #perforamce measure of 5, gives 5 movies before score can become negative
-#le1.parseUserResponse("Tarzan", False)  
