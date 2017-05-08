54257f94fe444f2e2f3f0bd51cea3d81774d7c03
Kenton <huangkenton@gmail.com>
2017-05-07 19:52:59 -0700

    return movie method commented out Test.py so i can use HashTableTester.py

--- HashTableTester.py
+++ HashTableTester.py
@@ -1,9 +1,23 @@
 import HashTable as h
+import Comparison as comp
+import fetcher as f
+
+fet = f.Fetcher()
+moviesInThreates = []
+fet.fetch(moviesInThreates, 15, 'theater')
+
 L = ["Action", "Drama", "Comedy"] #create List of genres as strings 
 Hash = h.HT(L) #create KB
-print Hash.Hello() # check if Genres added to hash table 
+print Hash.Hello() # check if Genres added to hash table
 Hash.updateProbs(L) # update probabilties -- testing function 
-R = ["Action", "Family"] # add new genre using update function 
+R = ["Action", "Family", "Adventure"] # add new genre using update function
 Hash.updateProbs(R) # call update function 
-print Hash.getProbability("Action") # Test getProbabilty function 
-print Hash.Hello() # check results 
+print Hash.getProbability("Action") # Test getProbabilty function
+print Hash.Hello() # check results
+
+movie = comp.returnMovie(moviesInThreates, Hash)
+for key, value in movie.iteritems():
+    print key.getName()
+    print key.getDate()
+    print key.getGenres()
+    print movie[key][1]
--- Test.py
+++ Test.py
@@ -2,18 +2,19 @@ import HashTable as h
 import LearningElement
 import user_input
 import fetcher
-
-user = user_input.NewUser()
-Hash = h.HT([]) 
-foundMovie = False
-runCount = 0
-while foundMovie == False:
-    runCount = runCount+1
-    print ""
-    print "-------------------------------------------"
-    print "Run:", runCount, " Performance Measure:", user.performanceMeasure
-    user_movie_genre = user.get_genre()
-    Hash.updateProbs(user_movie_genre) 
-    print user_movie_genre
-    print Hash.Hello() 
-
+import Comparison as c
+#
+# user = user_input.NewUser()
+# Hash = h.HT([])
+# foundMovie = False
+# runCount = 0
+# while foundMovie == False:
+#     runCount = runCount+1
+#     print ""
+#     print "-------------------------------------------"
+#     print "Run:", runCount, " Performance Measure:", user.performanceMeasure
+#     user_movie_genre = user.get_genre()
+#     Hash.updateProbs(user_movie_genre)
+#     print user_movie_genre
+#     print Hash.Hello()
+#
