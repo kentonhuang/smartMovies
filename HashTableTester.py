400fbd032fb0da2123d7463ba48ef3371700abcb
Wai Myint <hsueh@waimyint.com>
2017-05-11 11:54:26 -0700

    smartFetch: -call it by     Hash = HashTable() //hashtable object that Jorge made     f = fetcher() //Fetcher object     smartList = []     f.smartFetch(Hash, smartList, 3) // Will add 3 movies that has the top 2 genres from the Hash table
    -will have to pass the most recently updated HashTable
    -also check to make sure that the movies aren't presented twice to the user

--- HashTableTester.py
+++ HashTableTester.py
@@ -1,4 +1,6 @@
 import HashTable as h
+import fetcher as f
+
 L = ["Action", "Drama", "Comedy"] #create List of genres as strings 
 Hash = h.HT(L) #create KB
 print Hash.Hello() # check if Genres added to hash table 
@@ -8,3 +10,14 @@ Hash.updateProbs(R) # call update function
 print Hash.getProbability("Action") # Test getProbabilty function 
 print Hash.Hello() # check results 
 print Hash.topGenre() # top genre returned 
+print Hash.secondTopGenre()
+
+
+smartMovies = []
+fet = f.Fetcher()
+fet.smartFetch(Hash, smartMovies, 3)
+fet.printMovieList(smartMovies)
+
+fet.smartFetch(Hash, smartMovies, 5)
+fet.printMovieList(smartMovies)
+
--- fetcher.py
+++ fetcher.py
@@ -6,22 +6,75 @@ import requests
 import apikey as api
 import movie
 
+
+
 class Fetcher:
-
-    popular = 0
-    popular_page = 1
     theater = 0
+    popular = 0
+    smart = 0
+    popular_page = 1
     theater_page = 1
+    smart_page = 1
     genresT = []
+    smart_genresT = []
 
-    def __init_(self):
-        self.theater = 0
-        self.popular = 0
+    def __init__(self):
+        pass
+
+    def smartFetch(self, hashTable, movieList, a=1):
+        if hashTable is None or movieList is None:
+            print "Please pass a initiated hashtable or movieList"
+            return False
+
+        response = requests.get(api.base + api.popular + api.v3 + api.end + str(self.smart_page))
+        ct = self.smart
+
+        if response.status_code != 200:
+            print "API fetch failed"
+            return False
+
+        data = response.json()
+        top = hashTable.topGenre()
+        second = hashTable.secondTopGenre()
+
+        x = 0
+        while x < a:
+            if ct < len(data['results']):
+                genres = []
+                for y in data['results'][ct]['genre_ids']:  # get genre(s) of current movie
+                    gName = (b for b in api.genre if b['id'] == y).next()['name']
+                    genres.append(gName)
+                if top in genres and second in genres:  # check if top & second genre is in the current movie Genre list
+                    # movie has the top and second genre, complete gathering information
+                    name = data['results'][ct]['original_title']
+                    date = data['results'][ct]['release_date']
+                    m = movie.Movie(name, genres, date)
+                    movieList.append(m)  # add it to the movieList passed in the funct. arguments
+                    if top not in self.smart_genresT:
+                        self.smart_genresT.append(top)
+                    if second not in self.smart_genresT:
+                        self.smart_genresT.append(second)
+                    x += 1
+                self.smart += 1
+                ct = self.smart
+            else:
+                if self.smart_page > data['total_pages']:
+                    print "Cannot pull anymore movies.. ran out"
+                    return False
+                self.smart_page += 1
+                self.smart = 0
+                response = requests.get(api.base + api.popular + api.v3 + api.end + str(self.smart_page))
+                ct = self.smart
+
+                if response.status_code != 200:
+                    print "API fetch failed"
+                    return False
+
+                data = response.json()
 
     def fetch(self, movieList, a=1, type='theater'):
         if movieList is None:
             return False
-
 
         if type is 'theater':
             response = requests.get(api.base + api.theater + api.v3 + api.end + str(self.theater_page))
@@ -55,7 +108,7 @@ class Fetcher:
                 ct += 1
                 x += 1
                 ##print "fetch successful, # of movies in the list "+str(type) +""
-                #print " # of genres: " +str(len(self.genresT))
+                # print " # of genres: " +str(len(self.genresT))
             else:
                 if type is 'theater':
                     if self.theater_page > data['total_pages']:
@@ -79,8 +132,6 @@ class Fetcher:
                     print "API fetch error"
                     return False
 
-
-
     def printMovieList(self, movieList):
         for x in movieList:
             print "Movie: " + x.getName() + "\n Genre: " + ', '.join(
@@ -89,4 +140,3 @@ class Fetcher:
     def printGenres(self):
         print "Total Genres: " + str(len(self.genresT)) + ""
         print "Genres: " + ', '.join(self.genresT)
-
