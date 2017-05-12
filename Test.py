68630d35363cdc45be891003ceac00778c57ceef
Soneer Sainion <soneer.sainion@sjsu.edu>
2017-05-12 11:27:15 -0700

    Add files via upload
    Uses smart fetch and Kentons Logic

--- Test.py
+++ Test.py
@@ -1,20 +1,33 @@
 import HashTable as h
+
+import Comparison as comp
 import LearningElement
 import user_input
 import fetcher
-import Comparison as c
-#
-# user = user_input.NewUser()
-# Hash = h.HT([])
-# foundMovie = False
-# runCount = 0
-# while foundMovie == False:
-#     runCount = runCount+1
-#     print ""
-#     print "-------------------------------------------"
-#     print "Run:", runCount, " Performance Measure:", user.performanceMeasure
-#     user_movie_genre = user.get_genre()
-#     Hash.updateProbs(user_movie_genre)
-#     print user_movie_genre
-#     print Hash.Hello()
-#
+
+user = user_input.NewUser()
+moviesInThreates = user.get_movies_threates()
+Hash = h.HT([]) 
+foundMovie = False
+runCount = 0
+while foundMovie == False:
+    runCount = runCount+1
+    print ""
+    print "-------------------------------------------"
+    print "Run:", runCount, " Performance Measure:", user.performanceMeasure
+    user_movie_genre = user.get_genre(Hash)
+    if user_movie_genre!= False:
+        Hash.updateProbs(user_movie_genre) 
+    
+        #Input moviesInTheater list, and Hash Table with genres returns a Movie Object
+        movie = comp.returnMovie(moviesInThreates, Hash)
+        for key, value in movie.iteritems():
+        #How to get movie info
+            print "      Name:", key.getName()
+            print "      Date:", key.getDate()
+            print "      Genres:", key.getGenres()
+            #Gets movie's prob
+            print "      Prob:", movie[key][0]
+            #Gets movie's true/false above/below threshold boolean
+            print "      Recommended:",movie[key][1]
+
--- user_input.py
+++ user_input.py
@@ -8,43 +8,69 @@ NewUser ##A new user class
 
 import difflib
 import fetcher
-
+import HashTable as h
 class NewUser:
     #List of movie genres as stated on IMBD
     #List of user choosen genre that will be returned
    
-    performanceMeasure = 5;
+    performanceMeasure = 2
     users_movie_genre_valid = []
     movieList = []
     moviePresented = [];
     f = fetcher.Fetcher()
     moviesInThreates = []
-    f.fetch(moviesInThreates, 15, 'theater')
-    
+    f.fetch(moviesInThreates, 8, 'theater')
+    f.printMovieList(moviesInThreates)
+    Hash = h.HT([]) 
     def get_movies(self):
         print "Select Which Movie You Like, Type N for None"
         self.f.printMovieList()
         
-    def get_genre(self):
+    def get_genre(self, HashINC):
         #Prints welcome message and shows user list of valid genres
+        self.Hash = HashINC
+        if self.performanceMeasure < 0: 
+            print "Smart Fetch" 
+            self.f.smartFetch(self.Hash, self.movieList, 3) # Will add 3 movies that has the top 2 genres from the Hash table
         
-        self.f.fetch(self.movieList, 5, 'popular')
+        else:
+            print "Normal Fetch"
+            self.f.fetch(self.movieList, 3, 'popular')
         print "Do you like any of these movies? "
         count = 0
         for x in self.movieList: 
             if(x not in self.moviesInThreates):
-                print count, x.getName()
+                print "    ",count, x.getName()
                 count = count + 1
         print ""
-        print("Please enter in comma separated list.")
+        print("    Please enter in comma separated list.")
     
         #Get user input, valid input is in a comma separated list
-        users_movie_genre_str = raw_input(">")
-
-        #Convert user input to list form
-        list_of_movies_liked = users_movie_genre_str.split(",")
-        
-        if(len(list_of_movies_liked) > 2):
+        correctFormat = False;
+        while correctFormat == False:
+            correctFormat = True
+            users_movie_genre_str = raw_input("    >")
+            if users_movie_genre_str == "":
+                self.movieList = []     
+                self.performanceMeasure = self.performanceMeasure - 1
+                return False
+            #Convert user input to list form
+            list_of_movies_liked = users_movie_genre_str.split(",")
+            for value in list_of_movies_liked:
+                try:
+                    w = int(value.lstrip())
+                    if w < 0 and w <3:
+                        correctFormat = False 
+                except ValueError as verr:
+                    correctFormat= False
+                    print "    ",verr       
+              
+            if correctFormat == False:
+                 print("    Please enter in comma separated list.")
+                 print("    Example: ")
+                 print("    >0,2")
+    
+        if(len(list_of_movies_liked) > 1):
             self.performanceMeasure = self.performanceMeasure + 1
         else:          
             self.performanceMeasure = self.performanceMeasure - 1
@@ -61,7 +87,9 @@ class NewUser:
         self.movieList = []
         return list_of_all_genres
      
-            
+    def get_movies_threates(self):
+        return self.moviesInThreates 
+        
     def get_movie_list(self):
         return self.movieList 
 #______________________________________________________________________________
